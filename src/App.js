952baa54a753c3cde2113b3d07d9cf727e59b090
eckoCode <joao.marques.code@gmail.com>
2020-09-12 16:43:11 +0100

    First commit with some static content

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <input/>
+      <button>Ez</button>
+    </div>
+  );
+}
+
+export default App;
