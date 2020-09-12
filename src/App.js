3dcf7ecb3269921e61317fbf540ed52d39a0bc11
eckoCode <joao.marques.code@gmail.com>
2020-09-12 17:30:24 +0100

    Add state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [todos, setTodos] = useState(['Study for some time','Take a shower']);
+
+
   return (
     <div className="App">
       <h1>Hello World!</h1>
       <input/>
       <button>Ez</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
     </div>
   );
 }
