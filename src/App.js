f7a0a42cc1bfdb9c8eb76c3729117ccc3acbeebe
eckoCode <joao.marques.code@gmail.com>
2020-09-12 17:47:16 +0100

    Add todo function

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import './App.css';
 
 function App() {
   
-  const [todos, setTodos] = useState(['Study for some time','Take a shower']);
+  const [todos, setTodos] = useState(['Study for some time','Take a shower','Programing is hard work']);
+  const [input, setInput] = useState('');
 
+  const addTodo = (event) =>{
+    //triger when click the button
+    event.preventDefault();
+    setTodos([...todos, input]);
+  }
 
   return (
     <div className="App">
       <h1>Hello World!</h1>
-      <input/>
-      <button>Ez</button>
-
+      <form>
+        <input value={input} onChange={event => setInput(event.target.value)}/>
+        <button type="submit" onClick={addTodo}>Add Todo</button>
+      </form>
       <ul>
         {todos.map(todo => (
           <li>{todo}</li>
